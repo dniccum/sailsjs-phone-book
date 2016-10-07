a5a3e9bbb7301aaa5e210057f263403c3ce68c21
Doug Niccum <dniccumdesign@gmail.com>
2016-10-06 21:04:24 -0500

    Adds number controller and model.

---
+++ api/models/Number.js
@@ -0,0 +1,28 @@
+/**
+ * Number.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    phoneNumber: {
+      type: 'string',
+      required: true,
+      minLength: 10,
+      maxLength: 12
+    },
+    label: {
+      type: 'string',
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      defaultsTo: 'Work'
+    },
+    person: {
+      model: person
+    }
+  }
+};
