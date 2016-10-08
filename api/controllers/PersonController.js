9e0eac70ea16e29ee00e0f5b757117902891b4f7
Doug Niccum <dniccumdesign@gmail.com>
2016-10-08 14:55:04 -0500

    Adds create and delete methods.

--- api/controllers/PersonController.js
+++ api/controllers/PersonController.js
@@ -8,8 +8,38 @@
 module.exports = {
 
 	createPerson: function(req, res) {
-		console.log("Created a person");
-		return res.ok();
+		Person.create({
+			name: req.param('name'),
+			company: req.param('company')
+		}).exec(function(error, response) {
+			if (error) return res.badRequest(error);
+
+			sails.log('New record created: ', JSON.stringify(response));
+			return res.ok(response);
+		});
+	},
+	delete: function(req, res) {
+		var id = req.param('id');
+
+		Person.findOne({
+			id: id
+		}).exec(function(error, personInfo) {
+			if (!personInfo) return res.notFound();
+
+			PhoneNumber.destroy({
+				person: id
+			}).exec(function(phoneError) {
+				if (phoneError) return res.negotiate(phoneError);
+				sails.log('All numbers deleted assoc. with: ', id);
+
+				Person.destroy(id).exec(function(personError) {
+					if (personError) return res.badRequest(personError);
+
+					sails.log('Person with id deleted: ', id);
+					return res.ok();
+				});
+			});
+		});
 	}
 
 };
