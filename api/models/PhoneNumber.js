96756da55cf5ab9008752a492730f74c7b3e8828
Doug Niccum <dniccumdesign@gmail.com>
2016-10-08 15:03:28 -0500

    Updates file description.

--- api/models/PhoneNumber.js
+++ api/models/PhoneNumber.js
@@ -1,7 +1,7 @@
 /**
  * Number.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: Server-side logic for managing phone numbers
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
